af25377448e8fe97ac52378b1e8cd6576d11e748
Swapnil Singh <swapnilskumars@gmail.com>
2024-01-12 16:49:00 +0530

    made changes and updated to feedbacksystemfirstyear

--- includes/config.php
+++ includes/config.php
@@ -1,15 +1,11 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'firstyear');
-$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'feedbacksystemfirstyear');
+$con = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 
 // Check connection
-if (mysqli_connect_errno())
-{
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
- }
-
-?>
-
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
